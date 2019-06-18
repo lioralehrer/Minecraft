efba317783c8f3b1671e3d1661c7cfd689191a02
Liora <lioralehrer@gmail.com>
2019-06-18 11:40:22 +0300

    .world{width:800px} for nice line + insert tile image: grass

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function insert() {
     let world = $(".world");
-    for (let i = 0; i < 900; i++) {
+    for (let i = 0; i < 1000; i++) {
         let tile = $("<div>");
         tile.addClass("tile");
         tile.attr("id",i);
-        tile.css("background-image", 'url("/images/cell/gold.png")');
+        tile.css("background-image", 'url("images/cell/grass.png")');
         world.append(tile);
 
     }
