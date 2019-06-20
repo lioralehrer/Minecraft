36a639c2d37adadd3e74eab1c374a60d2b3960a1
Liora <lioralehrer@gmail.com>
2019-06-20 22:13:25 +0300

    click send to function work which sends to func minecraft or builTile

--- js/entities/tools/tool.js
+++ js/entities/tools/tool.js
@@ -4,5 +4,10 @@ class Tool{
         this.worksOn = worksOn;
         
     }
-  
+    mine(){
+        
+        // take off the css bacground & keep it
+        // can I do it from here?
+        
+    }
 }
--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
-let tools = [new Axe(), new Pickaxe(), new Shovel(), new Builder()];
+let tools = [new Axe(), new Pickaxe(), new Shovel(),new Builder()];
 let currentTool = null;
 let keeper = null;
-// let build = null;
+let matter;
 let inventory = [];
-let builder = $("#keeper");
+let builder = $("#builder");
 // let classList = builder.attr("class").split(' ');
 
 
@@ -19,32 +19,107 @@ $("document").ready(function () {
     }
     pickTool();
 
-    $(".tile").click(mine);
-
-    function mine(e) {
-        let matter = e.target.className.split(' ').pop();
+    $(".tile").click(work);
+    function work(e) {
         if (currentTool !== null) {
-            if (currentTool.worksOn[0] === matter) {
-                keeper = matter;
-                $(e.target).removeClass(matter);
-            } else {
-                if (currentTool.worksOn[1] === matter) {
-                    keeper = matter;
-                    $(e.target).removeClass(matter);
-                }
+            if (currentTool.type !== "builder") {
+                minecraft(e);
+            }
+            else {
+                console.log("inside work>else buildTile");
+                buildTile(e);
             }
         }
-
+    };
+    function minecraft(e) {
+        let matter = e.target.className.split(' ').pop();
+        if (currentTool.worksOn[0] === matter) {
+            keeper = matter;
+            $(e.target).removeClass(matter);
+        } else {
+            if (currentTool.worksOn[1] === matter) {
+                keeper = matter;
+                $(e.target).removeClass(matter);
+            }
+        }
         if (keeper !== null && keeper === matter && (currentTool.worksOn[0] === matter || currentTool.worksOn[1] === matter)) {
             inventory.push(keeper);
             console.log(inventory);
-            let lastMatter = inventory[inventory.length - 1];
+            lastMatter = inventory[inventory.length - 1];
             console.log("lastMatter is " + lastMatter);
             builder.removeClass();
-            builder.addClass(" build-tile  tool "+lastMatter);
+            builder.addClass(" build-tile  tool " + lastMatter);
         }
-             
-    };
+
+    }
+    function buildTile(e){
+        console.log("inside work>else buildTile");
+        let matter = e.target.className.split(' ').pop();
+        if (matter === "tile" ){
+            $(e.target).addClass(inventory.pop());
+        }
+
+    }
+
+    // function mine(e) {
+    //     let matter = e.target.className.split(' ').pop();
+    //     if (currentTool !== null) {
+
+    //         if (currentTool.worksOn[0] === matter) {
+    //             keeper = matter;
+    //             $(e.target).removeClass(matter);
+    //         } else {
+    //             if (currentTool.worksOn[1] === matter) {
+    //                 keeper = matter;
+    //                 $(e.target).removeClass(matter);
+    //             }
+    //         }
+    //     }
+
+    //     if (keeper !== null && keeper === matter && (currentTool.worksOn[0] === matter || currentTool.worksOn[1] === matter)) {
+    //         inventory.push(keeper);
+    //         console.log(inventory);
+    //          lastMatter = inventory[inventory.length - 1];
+    //         console.log("lastMatter is " + lastMatter);
+    //         builder.removeClass();
+    //         builder.addClass(" build-tile  tool "+lastMatter);
+    //     }
+
+    // };
+    // function mine(e) {
+
+
+    //     if ((currentTool !== null) && (currentTool.type !== "builder"))   {
+    //         matter = e.target.className.split(' ').pop();
+    //         if (currentTool.worksOn[0] === matter) {
+    //             keeper = matter;
+    //             $(e.target).removeClass(matter);
+    //         } else {
+    //             if (currentTool.worksOn[1] === matter) {
+    //                 keeper = matter;
+    //                 $(e.target).removeClass(matter);
+    //             }
+    //         }
+    //     }
+
+    //     if (keeper !== null && keeper === matter
+    //         && (currentTool.worksOn[0] === matter || currentTool.worksOn[1] === matter
+    //             && currentTool.type !== "builder")) {
+    //         inventory.push(keeper);
+    //         console.log(inventory);
+    //         // let lastMatter = inventory[inventory.length - 1];
+    //         // console.log("lastMatter is " + lastMatter);
+    //         builder.removeClass();
+    //         builder.addClass(" buildTile  tool " + keeper);
+    //     }
+
+    //     if (currentTool.type === "builder") {
+    //         if (currentTool.worksOn[0] === matter) {
+    //             $(e.target).addClass(keeper);
+    //         }
+    //     }
+
+    // };
 
 
 });
