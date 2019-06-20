171d7c26dd432093b97fb8f8993dd18a900fe1b5
Liora <lioralehrer@gmail.com>
2019-06-20 17:46:00 +0300

    nothing

--- js/game.js
+++ js/game.js
@@ -3,19 +3,17 @@ let currentTool = null;
 let keeper = null;
 // let build = null;
 let inventory = [];
-
-function pickTool() {
-    for (let i = 0; i < tools.length; i++) {
-        $(`#${tools[i].type}`).click(function () {
-            currentTool = tools[i];
-        });
-    }
-}
-pickTool();
-
 $("document").ready(function () {
-    $(".tile").click(function (e) { work(e) });
-    function work(e) {
+    function pickTool() {
+        for (let i = 0; i < tools.length; i++) {
+            $(`#${tools[i].type}`).click(function () {
+                currentTool = tools[i];
+            });
+        }
+    }
+    pickTool();
+    $(".tile").click(mine);
+    function mine(e) {
         let matter = e.target.className.split(' ').pop();
         if (currentTool !== null) {
             if (currentTool.worksOn[0] === matter) {
@@ -27,37 +25,54 @@ $("document").ready(function () {
                     $(e.target).removeClass(matter);
                 }
             }
-            if (currentTool.type === "builder") {
-                building(e);
-            }
-
-            else {
-                build = keeper;
-                $(e.target).removeClass(keeper);
-                $("#builder").addClass(keeper);
-            };
-
-        };
+            
+        }
     }
-    function building(e) {
-        $(e.target).addClass(build);
-        $("#builder").removeClass(build);
-        build = null;
-    }
-            }           
-        //  } 
-        
+    
+}
+    
+//     function work(e) {
+//         let matter = e.target.className.split(' ').pop();
+//         if (currentTool !== null) {
+//             if (currentTool.worksOn[0] === matter) {
+//                 keeper = matter;
+//                 $(e.target).removeClass(matter);
+//             } else {
+//                 if (currentTool.worksOn[1] === matter) {
+//                     keeper = matter;
+//                     $(e.target).removeClass(matter);
+//                 }
+//             }
+//             if (currentTool.type === "builder") {
+//                 building(e);
+//             }
+
+//             else {
+//                 build = keeper;
+//                 $(e.target).removeClass(keeper);
+//                 $("#builder").addClass(keeper);
+//             };
+
+//         };
+//     }
+//     function building(e) {
+//         $(e.target).addClass(build);
+//         $("#builder").removeClass(build);
+//         build = null;
+//     }
+// }
+    //  } 
+
     //     if (keeper !== null && keeper === matter && currentTool.worksOn[0] === matter) {
     //         inventory.push(keeper);            
     //         console.log(inventory);
     //         let lastMatter = inventory[inventory.length - 1];
     //         console.log("lastMatter is " + lastMatter);
-            
+
     //         $("#keeper").addClass(lastMatter);
     //     }
-            
-    // };
-// });
 
-// $("#keeper").addClass("keeper");
-);
+    // };
+    // });
+    
+    // $("#keeper").addClass("keeper");
